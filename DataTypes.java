edee676945ef974e2662db0979bf4000fe0c0d40
morapakulachandrika <166094673+morapakulachandrika@users.noreply.github.com>
2025-07-30 09:04:45 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+class DataTypesInteger
+{
+	public static void main(String args[])
+	{
+		int x=10; 		// public static java.lang.String toBinaryString(int) method;
+		System.out.println(Integer.toBinaryString(x));
+         int x1=-10; //toHexString(int);			 toOctalString(int);	 toString(int);
+	
+	     System.out.println(Integer.toBinaryString(x1));
+	}
+}
